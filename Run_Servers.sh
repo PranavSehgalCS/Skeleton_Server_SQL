79cc48ea97eef8a57998ee1ef55aff69e0d56a37
PranavSehgalCS <ps1958@rit.edu>
2023-06-19 14:46:48 +0530

    node_modules ADDED

--- Run_Servers.sh
+++ Run_Servers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-osascript -e 'tell application "Terminal" to do script "cd Desktop/P3F/Skeleton_Server/Project_API; mvn compile exec:java;mvn compile exec:java;"'
+osascript -e 'tell application "Terminal" to do script "cd Desktop/P3F/Skeleton_Server_SQL/Project_API; mvn compile exec:java;mvn compile exec:java;"'
 
 sleep 10;
 open  -a "Postgres"
